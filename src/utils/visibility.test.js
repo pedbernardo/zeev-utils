b5a2e8fd5f7d95926157bab68f7c47c6cdccbe7c
Pedro Bernardo <pedrovbernardo@gmail.com>
2022-04-26 22:53:42 -0300

    feat: add showGroup and hideGroup unit tests

--- src/utils/visibility.test.js
+++ src/utils/visibility.test.js
@@ -7,10 +7,13 @@ import { createField } from '../tests/dom'
 import {
   getFieldContainer
 } from './field'
+import { isRequired } from './require'
 
 import {
   showField,
-  hideField
+  hideField,
+  showGroup,
+  hideGroup
 } from './visibility'
 
 describe('showField', () => {
@@ -123,8 +126,126 @@ describe('hideField', () => {
   })
 })
 
-/**
- * @todo
- * - adicionar testes para showGroup
- * - adicionar testes para hideGroup
- */
+describe('showGroup', () => {
+  it('deve retornar undefined quando o parâmetro container for vazio ou não informado', () => {
+    const fields = showGroup('')
+    expect(fields).toBeUndefined()
+  })
+
+  it('deve retornar undefined quando o container não for encontrado', () => {
+    const fields = showGroup('#containerInexistente')
+    expect(fields).toBeUndefined()
+  })
+
+  it('deve retornar undefined quando o nenhum campo for encontrado', () => {
+    const containerId = 'containerExistente'
+
+    document.body.innerHTML = `<div id="${containerId}"></div>`
+
+    const fields = showGroup(`#${containerId}`)
+
+    expect(fields).toBeUndefined()
+  })
+
+  it('deve remover a classe auxiliar de visibilidade do container do grupo', () => {
+    const fieldId = 'meuCampo'
+    const containerClass = 'meu-container'
+    const hideClass = 'hidden'
+
+    document.body.innerHTML = `<div class="${containerClass} ${hideClass}">
+      ${createField({ id: fieldId, required: false })}
+    </div>`
+
+    const container = document.querySelector(`.${containerClass}`)
+
+    showGroup(`.${containerClass}`, { hideClass })
+
+    expect(container.classList.contains(hideClass)).toBeFalsy()
+  })
+
+  it('deve re-adicionar o atributo required="S" em todos os campos com a propriedade data-was-required', () => {
+    const fieldId = 'meuCampo'
+    const containerClass = 'meu-container'
+
+    document.body.innerHTML = `<div class="${containerClass}">
+      ${createField({ id: fieldId, dataWasRequired: true })}
+      ${createField({ id: fieldId, dataWasRequired: true })}
+      ${createField({ id: fieldId, required: false })}
+    </div>`
+
+    const fields = showGroup(`.${containerClass}`)
+    const requiredFields = fields.filter(field => isRequired(field))
+
+    expect(requiredFields.length).toBe(2)
+  })
+})
+
+describe('hideGroup', () => {
+  it('deve retornar undefined quando o parâmetro container for vazio ou não informado', () => {
+    const fields = hideGroup('')
+    expect(fields).toBeUndefined()
+  })
+
+  it('deve retornar undefined quando o container não for encontrado', () => {
+    const fields = hideGroup('#containerInexistente')
+    expect(fields).toBeUndefined()
+  })
+
+  it('deve retornar undefined quando o nenhum campo for encontrado', () => {
+    const containerId = 'containerExistente'
+
+    document.body.innerHTML = `<div id="${containerId}"></div>`
+
+    const fields = hideGroup(`#${containerId}`)
+
+    expect(fields).toBeUndefined()
+  })
+
+  it('deve adicionar a classe auxiliar de visibilidade do container do grupo', () => {
+    const fieldId = 'meuCampo'
+    const containerClass = 'meu-container'
+    const hideClass = 'hidden'
+
+    document.body.innerHTML = `<div class="${containerClass} ${hideClass}">
+      ${createField({ id: fieldId, required: false })}
+    </div>`
+
+    const container = document.querySelector(`.${containerClass}`)
+
+    hideGroup(`.${containerClass}`, { hideClass })
+
+    expect(container.classList.contains(hideClass)).toBeTruthy()
+  })
+
+  it('deve remover o atributo required="S" em todos os campos obrigatórios', () => {
+    const fieldId = 'meuCampo'
+    const containerClass = 'meu-container'
+
+    document.body.innerHTML = `<div class="${containerClass}">
+      ${createField({ id: fieldId, required: true })}
+      ${createField({ id: fieldId, required: true })}
+      ${createField({ id: fieldId, required: true })}
+    </div>`
+
+    const fields = hideGroup(`.${containerClass}`)
+    const requiredFields = fields.filter(field => field.getAttribute('required') === 'S')
+
+    expect(requiredFields.length).toBe(0)
+  })
+
+  it('deve limpar os valores de todos dos campos', () => {
+    const fieldId = 'meuCampo'
+    const containerClass = 'meu-container'
+
+    document.body.innerHTML = `<div class="${containerClass}">
+      ${createField({ id: fieldId, required: true, value: 'teste1' })}
+      ${createField({ id: fieldId, required: true, value: 'teste2' })}
+      ${createField({ id: fieldId, required: true, value: 'teste3' })}
+    </div>`
+
+    const fields = hideGroup(`.${containerClass}`)
+    const requiredFields = fields.filter(field => field.value === '')
+
+    expect(requiredFields.length).toBe(3)
+  })
+})
